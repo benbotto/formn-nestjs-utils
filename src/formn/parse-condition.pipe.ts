fc18a5d4d1a131d670dfd9bd88bab530926a67cf
Ben Botto <benjamin.botto@gmail.com>
2019-10-14 15:48:39 -0700

    Pipe to parse a Formn condition from a query string.

---
+++ src/formn/parse-condition.pipe.ts
@@ -0,0 +1,32 @@
+import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
+
+import { ConditionLexer, ConditionParser } from 'formn';
+
+import { ValidationError } from 'bsy-error';
+
+@Injectable()
+export class ParseConditionPipe implements PipeTransform<any> {
+  /**
+   * Transform a string value to an object, but lex and parse it as a Formn
+   * condition.
+   */
+  transform(value: string, meta: ArgumentMetadata): object {
+    // The transformer only transforms single Formn entities.
+    if (typeof value !== 'string')
+      throw new ValidationError(`Invalid value type ("${meta.data}" must be a string).`, meta.type);
+
+    try {
+      const lexer  = new ConditionLexer();
+      const parser = new ConditionParser();
+
+      // If it lexes and parses, it's a valid condition object.
+      parser.parse(
+        lexer.parse(value));
+
+      return JSON.parse(value);
+    }
+    catch (err) {
+      throw new ValidationError(`"${meta.data}" must contain be a valid Formn condition: ${err.message}`, meta.type);
+    }
+  }
+}
