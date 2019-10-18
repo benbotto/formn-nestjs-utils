bdd08e8435020050a37b9f5261b23465ef70f562
Ben Botto <benjamin.botto@gmail.com>
2019-10-18 16:12:40 -0700

    Adds order.

--- src/formn/search/search-result.ts
+++ src/formn/search/search-result.ts
@@ -1,6 +1,9 @@
+import { OrderByType } from 'formn';
+
 export class SearchResult<T extends object> {
   count: number;
   offset: number;
   rowCount: number;
   entities: T[];
+  order: OrderByType[];
 }
