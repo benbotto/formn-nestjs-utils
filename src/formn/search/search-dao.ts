8689faff43a5547edd889652bc9de94d65ae8bc7
Ben Botto <benjamin.botto@gmail.com>
2019-11-08 11:43:30 -0800

    Adds the ability to search over multiple tables.

--- src/formn/search/search-dao.ts
+++ src/formn/search/search-dao.ts
@@ -1,6 +1,6 @@
 import {
   DataContext, EntityType, ParameterType, ParameterizedCondition, OrderByType,
-  metaFactory, ColumnMetadata
+  metaFactory, ColumnMetadata, ConditionBuilder, FromAdapter
 } from 'formn';
 
 import { SearchResult } from './search-result';
@@ -16,6 +16,35 @@ export class SearchDao<T extends object> {
     protected dataContext: DataContext,
     protected Entity: EntityType<T>,
     protected alias: string) {
+  }
+
+  /**
+   * Get a FromAdapter instance (an Query that can be select()'d and
+   * count()'d).  It should not include the where clause.
+   */
+  getSearchQuery(): FromAdapter<T> {
+    return this.dataContext
+      .from<T>(this.Entity, this.alias);
+  }
+
+  /**
+   * Get the single unique identifier for the base table.  This is only needed
+   * if multiple tables are joined, in which case there needs to be a way of
+   * uniquely identifying top-level entities that match the search criteria.
+   * The return value should be the property name only (e.g. "id" of "email").
+   */
+  getUniqueIdentifier(): string {
+    const pks = metaFactory
+      .getColumnStore()
+      .getPrimaryKey(this.Entity);
+
+    if (pks.length !== 1) {
+      throw new Error(
+        `A unique identifier cannot be derived for "${this.Entity.name}" ` +
+        'because it uses a composite primary key.');
+    }
+
+    return pks[0].mapTo;
   }
 
   /**
@@ -47,30 +76,52 @@ export class SearchDao<T extends object> {
     }
 
     return this.dataContext.beginTransaction(async () => {
-      const query = this.dataContext
-        .from<T>(this.Entity, this.alias);
+      const countQuery = this.getSearchQuery();
+      const uniqueId   = this.getUniqueIdentifier();
+      const fqUniqueId = ColumnMetadata.createFQName(this.alias, uniqueId);
 
       if (cond)
-        query.where(cond)
+        countQuery.where(cond);
 
-      // Count of entities matching the filter.
-      const qCount = query
-        .count()
+      // Count of top-level entities matching the filter.
+      const qCount = countQuery
+        .countDistinct(fqUniqueId)
         .execute();
 
-      // List of entities.
-      const qEntities = query
+      // Distinct top-level IDs matching the filter.
+      const qIDs = countQuery
+        .selectDistinct(fqUniqueId)
+        .orderBy(...order)
+        .limit(offset, rowCount)
+        .execute();
+
+      const [count, distinctEnts] = await Promise
+        .all([qCount, qIDs]);
+
+      // List of entities (pulled by ID).
+      const cb = new ConditionBuilder();
+      const ids = distinctEnts
+        .map(ent => (ent as ParameterType)[uniqueId]);
+      let idCond = cb
+        .in(fqUniqueId, ':search-distinct-ids', ids);
+
+      // The original filter is still needed because it may filter out
+      // child entities.
+      if (cond)
+        idCond = cb.and(idCond, cond);
+
+      // A new condition is used, so getSearchQuery is called again.
+      const entities = await this
+        .getSearchQuery()
+        .where(idCond)
         .select()
         .orderBy(...order)
         .limit(offset, rowCount)
         .execute();
 
-      const [count, entities] = await Promise
-        .all([qCount, qEntities]);
-
       // Search results.  Note that the number of rows returned (rowCount) may
       // be fewer than the number requested.
-      const result = new SearchResult<T>();
+      const result    = new SearchResult<T>();
 
       result.count    = count;
       result.rowCount = entities.length;
